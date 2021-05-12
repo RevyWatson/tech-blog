e9fd3bdb7af67bc3cb0f1ed49fa3db9d5345167a
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-05-12 11:20:47 -0400

    Updated js, handlebars, and css. Deleted html templates,

---
+++ public/js/blog.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const blogTitle = document.querySelector("#blogTitle").value.trim();
+    const blogContent = document.querySelector("#blogContent").value.trim();
+  
+    if (blogTitle && blogContent) {
+      const response = await fetch(`/api/blog`, {
+        method: "POST",
+        body: JSON.stringify({ blogTitle, blogContent }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace("/blog");
+      } else {
+        alert("Oof! Failed to create blog post");
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute("data-id")) {
+      const id = event.target.getAttribute("data-id");
+  
+      const response = await fetch(`/api/blog/${id}`, {
+        method: "DELETE",
+      });
+  
+      if (response.ok) {
+        document.location.replace("/blog");
+      } else {
+        alert("Oops! Failed to delete blog post!");
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blog-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blog-list')
+    .addEventListener('click', delButtonHandler);
